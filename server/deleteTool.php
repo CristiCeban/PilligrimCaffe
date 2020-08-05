5d410825000bfcc571d78ff7d5759faf91ebce78
CristiCeban <cebancristi4444@gmail.com>
2020-08-05 17:06:50 +0300

    added dashboard admin produs

--- server/deleteTool.php
+++ server/deleteTool.php
@@ -6,20 +6,8 @@ if(isset($_GET['idUtilizator'])) {
     $manager->deleteUser($_GET['idUtilizator']);
     $category='?category=Users';
 }
-elseif(isset($_GET['eid'])) {
-    $manager->deleteTeacher($_GET['eid']);
-    $category='?category=Teachers';
-}
-elseif(isset($_GET['mid'])) {
-    $manager->deleteModule($_GET['mid']);
-    $category='?category=Modules';
-}
-elseif(isset($_GET['gid'])) {
-    $manager->deleteGroup($_GET['gid']);
-    $category = '?category=Groups';
-}
-elseif(isset($_GET['gtid'])) {
-    $manager->deleteGtype($_GET['gtid']);
-    $category = '?category=Gtypes';
+elseif(isset($_GET['idProdus'])) {
+    $manager->deleteProduct($_GET['idProdus']);
+    $category='?category=Products';
 }
 header('Location: ../client/admin_dashboard.php'.$category);
