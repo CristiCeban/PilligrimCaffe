d41760be8063f2c6a38ab90e358cf5a88e94152b
CristiCeban <cebancristi4444@gmail.com>
2020-08-12 13:16:03 +0300

    refactor href to produse

--- client/shop_grid.php
+++ client/shop_grid.php
@@ -2,15 +2,9 @@
 
 include_once 'config/init.php';
 
-$manager = new Product();
 
 $template = new Template('templates/shop_grid.php');
-$template->categorie = "";
-if(isset($_GET['bc']))
-    $template->categorie = 'bc';
-if(isset($_GET['br']))
-    $template->categorie = 'br';
-if(isset($_GET['']))
-    $template->categorie = 'bauturi_calde';
-
+$template->categorie = isset($_GET['cat']) ? $_GET['cat'] : null;
+$template->manager = new Product();
+$template->result_set = $template->manager->getProductbyCategory($template->categorie);
 echo $template;
--- client/templates/shop_grid.php
+++ client/templates/shop_grid.php
@@ -5,6 +5,11 @@ require "../components/Header.php";
 echo $this->categorie;
 ?>
 
+<?php foreach($this->result_set as $item){
+    echo '<p'.$item->NumeProdus.'/>';
+}?>
+
+
 <?php
-require "../components/Footer.php"
+require "../components/Footer.php";
 ?>
