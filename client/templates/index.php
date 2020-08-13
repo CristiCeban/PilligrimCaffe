4a79808fe445e3d90b6079cefa6ebd29b547bd0b
Alex <alexwarofcommand@gmail.com>
2020-08-13 13:23:13 +0300

    side bar changes

--- client/templates/index.php
+++ client/templates/index.php
@@ -73,21 +73,17 @@ require "../components/Header.php"
                                 <button type="submit" class="site-btn" style="color:black;">Caută</button>
                             </form>
                         </div>
-                        <!-- <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
-                            </div>
-                        </div> -->
+                        
 
                     </div>
                 
                     
                         
                     
+                </div>
+
+                <div class="col-lg-2">
+                    <a style="color:orange !important"  href="./contact.php">Contacte</a>
                 </div>
                 <div class="col-lg-3">
                     <div class="menu-bar my-sm-1 ">
@@ -103,7 +99,7 @@ require "../components/Header.php"
             
         </div>
     </section>
-        <section>
+        <!-- <section>
                         <div class="side-menu" id="side-menu">
                             <ul>
                                 <li>Еда<i class="fa fa-angle-right"></i>
@@ -129,9 +125,48 @@ require "../components/Header.php"
                             <li>Авто<i class="fa fa-angle-right"></i></li>
                             </ul>
                         </div>
-            </section>
+            </section> -->
+            <div class="row">
+                <div class="col-lg-3">
+                <div class="contenedor-menu mx-1">
+		        <a href="" class="btnMenu">Menu <i class="fa fa-bars"></i></a>
 
-<!--Banner-->
+                <ul class="menu">
+                    <li><a href="bauturi calde">Caffea,ceai</a></li>
+                    <li><a href="Brutarie">Brutarie</a></li>
+                    <li><a href="#">Bauturi nealc <i class="fa fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="cafea rece">Cafea rece</a></li>
+                            <li><a href="apa">Apa potabila</a></li>
+                            <li><a href="sucuri">Sucuri</a></li>
+                            <li><a href="energizante">Energizante</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">Bauturi alc<i class="fa fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="vin">Vin</a></li>
+                            <li><a href="coniac">Coniac</a></li>
+                            <li><a href="vodka">Vodka</a></li>
+                            <li><a href="wiski">Wisky</a></li>
+                            <li><a href="bere">Bere</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li><a href="#">Produse <i class="fa fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="Snack-uri">Snack-uri</a></li>
+                            <li><a href="Zakuska la bere">La o bere</a></li>
+                            <li><a href="arahide">Arahide</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="tigari">Tigari</a></li>
+                    
+                </ul>
+	        </div>
+                </div>
+                <div class="col-lg-9">
+                    <!--Banner-->
 
 
     <div class="slider ">
@@ -143,13 +178,13 @@ require "../components/Header.php"
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="images/banner4.jpg" class="d-block w-100" alt="...">
+                <img class="carousel-img" src="images/banner4.jpg" class="d-block " alt="...">
             </div>
             <div class="carousel-item">
-                <img src="images/banner1.jpg" class="d-block w-100" alt="...">
+                <img class="carousel-img" src="images/banner1.jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="images/banner2.jpg" class="d-block w-100" alt="...">
+                <img class="carousel-img" src="images/banner2.jpg" class="d-block w-100" alt="...">
             </div>
         </div>
         <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
@@ -162,6 +197,10 @@ require "../components/Header.php"
         </a>
     </div>
     </div>
+                </div>
+            </div>
+            
+
 
 <!--Bannner end-->
     <!-- Hero Section End -->
@@ -230,6 +269,7 @@ require "../components/Footer.php"
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/main.js"></script>
     <script src= "js/bar-alter.js"></script>
+    <script src="js/sidebar-accordion.js"></script>
 
 
 </body>
