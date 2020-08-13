a4b4d0c2a1bf7569f95fa79cdafdb8f2a44e46c7
Alex <alexwarofcommand@gmail.com>
2020-08-13 17:30:58 +0300

    number type changed

--- client/templates/shoping-cart.php
+++ client/templates/shoping-cart.php
@@ -50,17 +50,15 @@ $sum = 0;
                                 $sum+=$result->Pret?>
                                 <tr>
                                     <td class="shoping__cart__item">
-                                        <img src="<?php echo $result->ImagePath ?>" alt="">
+                                        <img class="img-thumbnail rouded" src="<?php echo $result->ImagePath ?>" alt="">
                                         <h5><?php echo $result->NumeProdus?></h5>
                                     </td>
                                     <td class="shoping__cart__price" id="price<?php echo $result->idProdus?>">
                                         <?php echo $result->Pret .' MDL' ?>
                                     </td>
                                     <td class="shoping__cart__quantity">
-                                        <div class="quantity">
-                                            <div class="pro-qty">
-                                                <input oninput="sumOfProduct(<?php echo  $result->idProdus?>)" type="number" min="1" max="100" value="1" id="quantity<?php echo $result->idProdus?>">
-                                            </div>
+                                        <div class="quantity">                                           
+                                                <input oninput="sumOfProduct(<?php echo  $result->idProdus?>)" type="number" min="1" max="100" value="1" id="quantity<?php echo $result->idProdus?>">  
                                         </div>
                                     </td>
                                     <td class="shoping__cart__total" id="sum_item<?php echo $result->idProdus?>">
