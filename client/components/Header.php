531975d5e928336064f493ce73d708d1c73940d2
Cristian1001001 <trevor.tm21@gmail.com>
2020-08-14 13:11:20 +0300

    header index +2 file, tat zaebok

--- client/components/Header.php
+++ client/components/Header.php
@@ -25,7 +25,6 @@
 	<link rel="stylesheet" href="css/sidebar-accordion.css">
 	<!-- Demo CSS -->
 	<link rel="stylesheet" href="css/demo.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -138,63 +137,10 @@
     </div>
     
     <section class="hero">
-                    <div class="row mt-4">
+                    <div class="row mt-4 mr-0">
                         <div class="header__logo  col-lg-2 " >
                             <a href="./index.php">
                                 <img src="images/logo_color.png" alt="" style=" width: 190px;">
                             </a>
                         </div>
 
-                        <div class="col-lg-5 mt-4">
-                            <div class="hero__search my-sm-1 mt-lg-1">
-                                <div class="hero__search__form">
-                                    <form action="#"">
-                                        <input type="text" placeholder="Termen de căutat">
-                                        <button type="submit" class="site-btn" style="color:black;">Caută</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
-                                <div class="col-md-2 mt-4 mr-1">
-                                    <div class="hero__search__phone">
-                                        <div class="hero__search__phone__icon">
-                                            <i class="fa fa-phone"></i>
-                                        </div>
-                                        <div class="hero__search__phone__text">
-                                            <b><h6>+373123456789</h6></b>
-                                            <span>suport 24/7 </span>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                    <div class="col-lg-1 mr-5 mt-4">
-                                        <a style=
-                                           "color:black !important;
-                                            font-family: 'Montserrat', sans-serif;;
-                                            font-size: 20px;
-                                            font-weight: 900;"
-                                            href="./contact.php">
-                                            Contacte</a>
-                                    </div>
-
-
-                                    <div class="col-lg-1 ml-1 mt-4">
-                                        <div class="menu-bar my-sm-1 ">
-                                            <ul class="mt-sm-0">
-                                                <li> <a href="./shoping-cart.php"><i class="fa fa-shopping-cart"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-
-
-                    </div>
-    </section>
-
-        
-        <div class="humberger__open">
-            <i class="fa fa-bars"></i>
-        </div>
-    
-</header>
---
+++ client/components/end.php
@@ -0,0 +1,8 @@
+</div>
+</section>
+
+<div class="humberger__open">
+    <i class="fa fa-bars"></i>
+</div>
+
+</header>
