11a54c5e267715ff71c10d6e5c7492bf85aa608b
CristiCeban <cebancristi4444@gmail.com>
2020-08-14 13:15:28 +0300

    doing check-out

--- client/templates/order_processing.php
+++ client/templates/order_processing.php
@@ -4,32 +4,41 @@ require "../components/Header.php";
 ?>
 
 <!-- Breadcrumb Section Begin -->
-<section class="breadcrumb-section set-bg" data-setbg="https://developers.klarna.com/static/KCO-Header.png">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <div class="breadcrumb__text">
-                    <h2>Check-out</h2>
-                    <div class="breadcrumb__option">
-                        <a href="index.php">Pagina principală</a>
-                        <span>Check-out</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<!--<section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg"-->
+<!--         style="background-image: url(img/breadcrumb.jpg);">-->
+<!---->
+<!---->
+<!--    <div class="container my-4 shoping-cart">-->
+<!--        <div class="row">-->
+<!--            <div class="col-lg-12 text-center">-->
+<!--                <div class="breadcrumb__text">-->
+<!--                    <h2>Check-out</h2>-->
+<!--                    <div class="breadcrumb__option">-->
+<!--                        <a href="index.php">Pagina principală</a>-->
+<!--                        <span>Check-out</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</section>-->
+
+
 <!-- Breadcrumb Section End -->
 
 
 <div class="row">
-    <div class="col-lg-3" >
+    <div class="col-lg-3">
 <!--        style="background: #0f6674">-->
 
 
     </div>
-    <div class="col-lg-4 col-md-8 shadow p-3 mb-5 bg-white rounded" style="background: white">
-        <div class="container align-items-center justify-content-center">
+
+
+    <div class="col-lg-4 col-md-8 ">
+
+
+        <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
             <h3>
                 <span class="tag_delivery">1</span>
                 <span class="rand_tag_delivery">Detalii livrare </span>
@@ -39,7 +48,7 @@ require "../components/Header.php";
                 Adresa de livrare:
             </h2>
 
-            <div class="row">
+            <div class="row" style="margin-top: 15px">
                 <div class="col-md-6">
                     <input style="width: 90%" type="text" placeholder="Adresa">
                 </div>
@@ -49,10 +58,47 @@ require "../components/Header.php";
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <input style = "width: 95.5%" type="text" placeholder="Informatii suplimentarea,scara,etaj,bloc,safonier">
+                    <textarea style="width: 95.5%;margin-top: 30px" rows="4"  placeholder="Informatii suplimentarea,scara,etaj,bloc,safonier"></textarea>
                 </div>
             </div>
         </div>
+
+
+        <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
+            <h3>
+                <span class="tag_delivery">2</span>
+                <span class="rand_tag_delivery">Date Personale</span>
+            </h3>
+            <br>
+            <h2>
+                Adresa de livrare:
+            </h2>
+
+            <div class="row" style="margin-top: 15px">
+                <div class="col-md-6">
+                    <input style="width: 90%" type="text" placeholder="Nume">
+                </div>
+                <div class="col-md-6">
+                    <input style="width: 90%" type="text" placeholder="Prenume">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <input style="width: 95.5%;margin-top: 30px"" type="text" placeholder="Numar mobil">
+                </div>
+            </div>
+            <div class="row">
+                <div class="text-center">
+                    <button id="singlebutton" name="singlebutton" class="btn btn-primary form-control">Submit</button>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
     </div>
 
 
@@ -65,7 +111,7 @@ require "../components/Header.php";
     </div>
 
 
-    <div class="col-lg-3" >
+    <div class="col-lg-3">
 <!--         style="background: #17a2b8">-->
     </div>
 </div>
