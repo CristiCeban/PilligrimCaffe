08224f7170cf83654905febda3194adbdd6cb65b
CristiCeban <cebancristi4444@gmail.com>
2020-08-14 15:42:52 +0300

    check out

--- client/templates/order_processing.php
+++ client/templates/order_processing.php
@@ -1,6 +1,7 @@
 <?php
 
 require "../components/Header.php";
+require "../components/end.php";
 ?>
 
 <!-- Breadcrumb Section Begin -->
@@ -36,65 +37,65 @@ require "../components/Header.php";
 
 
     <div class="col-lg-4 col-md-8 ">
+        <form action="somewhere.php">
 
+            <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
+                <h3>
+                    <span class="tag_delivery">1</span>
+                    <span class="rand_tag_delivery">Detalii livrare </span>
+                </h3>
+                <br>
+                <h2>
+                    Adresa de livrare:
+                </h2>
 
-        <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
-            <h3>
-                <span class="tag_delivery">1</span>
-                <span class="rand_tag_delivery">Detalii livrare </span>
-            </h3>
-            <br>
-            <h2>
-                Adresa de livrare:
-            </h2>
-
-            <div class="row" style="margin-top: 15px">
-                <div class="col-md-6">
-                    <input style="width: 90%" type="text" placeholder="Adresa">
+                <div class="row" style="margin-top: 15px">
+                    <div class="col-md-6">
+                        <input style="width: 90%" type="text" placeholder="Adresa">
+                    </div>
+                    <div class="col-md-6">
+                        <input style="width: 90%" type="text" placeholder="orasul/satul">
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <input style="width: 90%" type="text" placeholder="orasul/satul">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <textarea style="width: 95.5%;margin-top: 30px" rows="4"  placeholder="Informatii suplimentarea,scara,etaj,bloc,safonier"></textarea>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
-            <h3>
-                <span class="tag_delivery">2</span>
-                <span class="rand_tag_delivery">Date Personale</span>
-            </h3>
-            <br>
-            <h2>
-                Adresa de livrare:
-            </h2>
-
-            <div class="row" style="margin-top: 15px">
-                <div class="col-md-6">
-                    <input style="width: 90%" type="text" placeholder="Nume">
-                </div>
-                <div class="col-md-6">
-                    <input style="width: 90%" type="text" placeholder="Prenume">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <input style="width: 95.5%;margin-top: 30px"" type="text" placeholder="Numar mobil">
-                </div>
-            </div>
-            <div class="row">
-                <div class="text-center">
-                    <button id="singlebutton" name="singlebutton" class="btn btn-primary form-control">Submit</button>
+                <div class="row">
+                    <div class="col-md-12">
+                        <textarea style="width: 95.5%;margin-top: 30px" rows="4"  placeholder="Informatii suplimentarea,scara,etaj,bloc,safonier"></textarea>
+                    </div>
                 </div>
             </div>
 
-        </div>
 
+            <div class="container align-items-center justify-content-center shadow p-3 mb-5 bg-white rounded" style="background: white">
+                <h3>
+                    <span class="tag_delivery">2</span>
+                    <span class="rand_tag_delivery">Date Personale</span>
+                </h3>
+                <br>
+                <h2>
+                    Adresa de livrare:
+                </h2>
+
+                <div class="row" style="margin-top: 15px">
+                    <div class="col-md-6">
+                        <input style="width: 90%" type="text" placeholder="Nume">
+                    </div>
+                    <div class="col-md-6">
+                        <input style="width: 90%" type="text" placeholder="Prenume">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <input style="width: 95.5%;margin-top: 30px"" type="text" placeholder="Numar mobil">
+                    </div>
+                </div>
+                <div class="container row">
+<!--                    <div class="text-center">-->
+                        <button type="submit" class="btn btn-primary btn-lg btn-block" style="width: 100%">Submit</button>
+<!--                    </div>-->
+                </div>
+
+            </div>
+        </form>
 
 
 
