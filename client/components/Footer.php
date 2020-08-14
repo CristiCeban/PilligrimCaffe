5e66ce0fc23e92a15eb6ffea3e3f254052d5aef6
Cristian1001001 <trevor.tm21@gmail.com>
2020-08-14 18:56:12 +0300

    language in contacte

--- client/components/Footer.php
+++ client/components/Footer.php
@@ -7,26 +7,76 @@
                         <a href="./index.php"><img src="images/logo_color.png" alt="" style="width: 180px"></a>
                     </div>
                     <ul>
-                        <li>Adresă: Str. Chișinăului, 68 Anenii Noi, Moldova/ Адрес: Ул. Кишинёвская, 68 Новые Анены, Молдова</li>
-                        <li>Telefon: /Телефон</li>
-                        <li>Email:/ Почта</li>
+                        <li>
+                            <?php if(check_lang_ru()):?>
+                            Адрес: Ул. Кишинёвская, 68 Новые Анены, Молдова
+                            <?php else:?>
+                            Adresă: Str. Chișinăului, 68 Anenii Noi, Moldova
+                            <?php endif;?>
+                        </li>
+
+                        <li>
+                            <?php if(check_lang_ru()):?>
+                            Телефон:
+                            <?php else:?>
+                            Telefon:
+                            <?php endif;?>
+                        </li>
+                        <li>
+                            <?php if(check_lang_ru()):?>
+                            Почта:
+                            <?php else:?>
+                            Email:
+                            <?php endif;?>
+                        </li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6 ">
                 <div class="footer__widget">
-                    <h6>Link-uri utile/ Полезные ссылки</h6>
+                    <h6>
+                        <?php if(check_lang_ru()):?>
+                        Полезные ссылки
+                        <?php else:?>
+                        Link-uri utile/
+                        <?php endif;?>
+                    </h6>
                     <ul>
-                        <li><a href="#">Despre Noi/ О нас</a></li>
-                        <li><a href="#">Despre Livrare/ Про доставку</a></li>
+                        <li><a href="#">
+                                <?php if(check_lang_ru()):?>
+                                О нас
+                                <?php else:?>
+                                Despre Noi
+                                <?php endif;?>
+                            </a></li>
+                        <li><a href="#">
+                                <?php if(check_lang_ru()):?>
+                                Про доставку
+                                <?php else:?>
+                                Despre Livrare
+                                <?php endif;?>
+                            </a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-6 ">
                 <div class="footer__widget">
-                    <h6>Program de lucru/ График работы</h6>
+                    <h6>
+                        <?php if(check_lang_ru()):?>
+                        График работы
+                        <?php else:?>
+                        Program de lucru
+                        <?php endif;?>
+                    </h6>
                     <ul>
-                        <li><a href="#">Luni - Duminică: 24/7/Понедельник - Воскресенье: 24/7</a></li>
+                        <li><a href="#">
+                                <?php if(check_lang_ru()):?>
+                                Понедельник - Воскресенье: 24/7
+                                <?php else:?>
+                                Luni - Duminică: 24/7
+                                <?php endif;?>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
--- client/templates/contact.php
+++ client/templates/contact.php
@@ -15,28 +15,64 @@ require "../components/end.php";
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
                         <span class="icon_phone"></span>
-                        <h4>Telefon</h4>
+                        <h4>
+                            <?php if(check_lang_ru()):?>
+                            Телефон
+                            <?php else:?>
+                            Telefon
+                            <?php endif;?>
+                        </h4>
                         <p>+01-3-8888-6868</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
                         <span class="icon_pin_alt"></span>
-                        <h4>Adresă</h4>
-                        <p>Str. Chișinăului, 68 Anenii Noi, Moldova</p>
+                        <h4>
+                            <?php if(check_lang_ru()):?>
+                            Адрес
+                            <?php else:?>
+                            Adresă
+                            <?php endif;?>
+                        </h4>
+                        <p>
+                            <?php if(check_lang_ru()):?>
+                                Адрес: Ул. Кишинёвская, 68 Новые Анены, Молдова
+                            <?php else:?>
+                                Adresă: Str. Chișinăului, 68 Anenii Noi, Moldova
+                            <?php endif;?>
+                        </p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
                         <span class="icon_clock_alt"></span>
-                        <h4>Deschis</h4>
-                        <p>Luni-Duminică: 24/7</p>
+                        <h4>
+                            <?php if(check_lang_ru()):?>
+                            Открыто
+                            <?php else:?>
+                            Deschis
+                            <?php endif;?>
+                        </h4>
+                        <p>
+                            <?php if(check_lang_ru()):?>
+                                Понедельник - Воскресенье: 24/7
+                            <?php else:?>
+                                Luni - Duminică: 24/7
+                            <?php endif;?>
+                        </p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
                         <span class="icon_mail_alt"></span>
-                        <h4>Email</h4>
+                        <h4>
+                            <?php if(check_lang_ru()):?>
+                            Почта
+                            <?php else:?>
+                            Email
+                            <?php endif;?>
+                        </h4>
                         <p>hello@colorlib.com</p>
                     </div>
                 </div>
@@ -62,21 +98,27 @@ require "../components/end.php";
             <div class="row">
                 <div class="col-lg-12">
                     <div class="contact__form__title">
-                        <h2>Lasa un mesaj</h2>
+                        <h2>
+                            <?php if(check_lang_ru()):?>
+                            Оставьте сообщение
+                            <?php else:?>
+                            Lasa un mesaj
+                            <?php endif;?>
+                        </h2>
                     </div>
                 </div>
             </div>
             <form action="#">
                 <div class="row">
                     <div class="col-lg-6 col-md-6">
-                        <input type="text" placeholder="Numele tau">
+                        <input type="text" <?php if(check_lang_ru()):?>placeholder="Имя"<?php else:?> placeholder="Numele tau"<?php endif;?>>
                     </div>
                     <div class="col-lg-6 col-md-6">
-                        <input type="email" placeholder="Email-ul tau">
+                        <input type="text" <?php if(check_lang_ru()):?>placeholder="Введите свой email"<?php else:?> placeholder="Emailul tău"<?php endif;?>>
                     </div>
                     <div class="col-lg-12 text-center">
-                        <textarea placeholder="Mesajul tau"></textarea>
-                        <button type="submit" class="site-btn">Trimite mesaj</button>
+                        <textarea <?php if(check_lang_ru()):?> placeholder="Ваше сообщение" <?php else:?>placeholder="Mesajul tau"<?php endif;?>></textarea>
+                        <button type="submit" class="site-btn"> <?php if(check_lang_ru()):?>Отправить сообщение<?php else:?>Trimite mesaj<?php endif;?></button>
                     </div>
                 </div>
             </form>
