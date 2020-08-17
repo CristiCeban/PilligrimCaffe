64b4332213a615a64ec667c6aa45ef373542bfe5
CristiCeban <cebancristi4444@gmail.com>
2020-08-17 11:41:28 +0300

    autoload fix

--- client/config/init.php
+++ client/config/init.php
@@ -3,8 +3,11 @@
 require_once 'db_config.php';
 require_once 'config.php';
 require_once '../lib/functions.php';
-spl_autoload_register('__autoload');
-function __autoload($class_name){
-    require_once "../class/".$class_name.".php";
-}
+//spl_autoload_register('__autoload');
+//function __autoload($class_name){
+//    require_once "../class/".$class_name.".php";
+//}
 
+spl_autoload_register(function ($class) {
+    include '../class/' . $class . '.php';
+});
